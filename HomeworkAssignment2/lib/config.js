8db20bab063aaad24b92ce3e2174672a4ab7319b
Maninder Singh Gill <msgill1989@gmail.com>
2019-07-17 13:13:48 +0530

    changes to config.js

--- HomeworkAssignment2/lib/config.js
+++ HomeworkAssignment2/lib/config.js
@@ -12,11 +12,11 @@ config.environment={
     "httpsPort": 3001,
     "hashingSecrete":"hashingSecret1",
     "stripeApi":{
-        "secretKey":"sk_test_oBfW5G1NQJPEGsiQsy1CEQWm00O2DPJVj4",
+        "secretKey":"sk_test_XXX",
         "sourceToken":"tok_visa"
     },
     "mailGun":{
-        "key":"api:f50306b07d5dfff0097c6efb626ac689-2b0eef4c-c5f86c4a",
+        "key":"api:XXXXXX",
         "from":"pizzaApp@gillms89.com"
     }
 }
